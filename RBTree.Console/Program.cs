5a15c704fe11ed8a95e65d3ea0ba593fe77584ec
Kamil Baldyga <kbaldyga@gmail.com>
2015-08-25 11:22:00 +0200

    finished test console app

--- RBTree.Console/Program.cs
+++ RBTree.Console/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Akka.Util.Internal.Collections;
-using RBTree;
-
 
 namespace RBTree.Console
 {
@@ -23,25 +20,26 @@ namespace RBTree.Console
 			Execute("rb  insert", () => { randoms.ForEach(r => tree = tree.Add(r)); return tree; });
 			Execute("rb2  insert", () => { randoms.ForEach(r => tree2 = tree2.Add(r)); return tree2; });
             Execute("set insert", () => { randoms.ForEach(r => set = set.Add(r)); return set; });
-            //var fset = new Microsoft.FSharp.Collections.FSharpSet<int>(Enumerable.Empty<int>());
-            //Execute("fset insert", () => { randoms.ForEach(r => fset = fset.Add(r)); return fset; });
-            //var immSet = System.Collections.Immutable.ImmutableHashSet<int>.Empty;
-            //Execute("immset insert", () => { randoms.ForEach(r => immSet = immSet.Add(r)); return set; });
+            var fset = new Microsoft.FSharp.Collections.FSharpSet<int>(Enumerable.Empty<int>());
+            Execute("fset insert", () => { randoms.ForEach(r => fset = fset.Add(r)); return fset; });
+            var immSet = System.Collections.Immutable.ImmutableHashSet<int>.Empty;
+            Execute("immset insert", () => { randoms.ForEach(r => immSet = immSet.Add(r)); return set; });
 
             System.Console.WriteLine();
-            //randoms = Enumerable.Range(0, tests).Select(_ => random.Next()).ToList();
             // LOOKUP
 			Execute("rb  contains", () => { randoms.ForEach(r => tree.Contains(r)); return tree; });
 			Execute("rb2  contains", () => { randoms.ForEach(r => tree2.Contains(r)); return tree2; });
             Execute("set  contains", () => { randoms.ForEach(r => set.Contains(r)); return tree; });
+            Execute("fset  contains", () => { randoms.ForEach(r => fset.Contains(r)); return tree; });
+            Execute("immset  contains", () => { randoms.ForEach(r => immSet.Contains(r)); return tree; });
 
 			System.Console.WriteLine();
             // DELETE
 			Execute("rb  remove", () => { randoms.ForEach(r => tree = tree.Remove(r)); return tree; });
 			Execute("rb2  remove", () => { randoms.ForEach(r => tree2 = tree2.Remove(r)); return tree2; });
             Execute("set  remove", () => { randoms.ForEach(r => set = set.Remove(r)); return tree; });
-            //Execute("rb  remove", () => { randoms.ForEach(r => fset = fset.Remove(r)); return tree; });
-            //Execute("rb  remove", () => { randoms.ForEach(r => immSet = immSet.Remove(r)); return tree; });
+            Execute("fset  remove", () => { randoms.ForEach(r => fset = fset.Remove(r)); return tree; });
+            Execute("immset  remove", () => { randoms.ForEach(r => immSet = immSet.Remove(r)); return tree; });
             
         }
 
